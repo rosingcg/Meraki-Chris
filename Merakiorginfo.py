08d7956817f64598a07c514a5cddac190372f688
rosingcg <33352171+rosingcg@users.noreply.github.com>
2017-11-06 15:20:26 -0600

    meraki

--- Merakiorginfo.py
+++
@@ -1,8 +0,0 @@
-from meraki import meraki
-from pprint import pprint
-from vars import apikey
-
-#apidata is a variable we are created to return apidata
-meraki_orgaccess = meraki.myorgaccess(apikey,suppressprint="true")
-#print(meraki_orgaccess)
-pprint(meraki_orgaccess)
